6e57b7fe50cbdebb3af31fafa0dcd23184c3a5ef
Samarash Eugene <samarasha95@gmail.com>
2025-01-24 21:34:37 +0200

    task is implemented

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const NEW_HISTORY = [];
+  const NEW_STATE = Object.assign({}, state);
+
+  for (const action in actions) {
+    let initialState = {
+      ...(NEW_HISTORY[NEW_HISTORY.length - 1] || NEW_STATE),
+    };
+
+    if (actions[action].type === 'clear') {
+      initialState = {};
+      NEW_HISTORY.push({ ...initialState });
+      continue;
+    }
+
+    if (actions[action].type === 'addProperties') {
+      for (const key in actions[action].extraData) {
+        initialState[key] = actions[action].extraData[key];
+      }
+      NEW_HISTORY.push({ ...initialState });
+    }
+
+    if (actions[action].type === 'removeProperties') {
+      for (const key of actions[action].keysToRemove) {
+        delete initialState[key];
+      }
+      NEW_HISTORY.push({ ...initialState });
+    }
+  }
+
+  return NEW_HISTORY;
 }
 
 module.exports = transformStateWithClones;
